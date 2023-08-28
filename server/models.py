e4c6c8a3b007c6f4ffde6fe3486253b2bb002ce2
MORGANJASON <420morganjason@gmail.com>
2023-08-28 16:25:15 +0300

    Created user model

---
+++ server/models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    user_id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String, unique=True)
+    lastName = db.Column(db.String, unique=True)
+    role = db.Column(db.String, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    phone_number = db.Column(db.String(10), nullable=False)
+    
