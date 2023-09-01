d19b6611faa86e0715e996b1736e1318bde84d5e
Medrine-Jepkemoi <99050771+Medrine-Jepkemoi@users.noreply.github.com>
2023-09-01 14:11:35 +0300

    Serializaton on user model

--- server/models.py
+++ server/models.py
@@ -1,8 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
     
     user_id = db.Column(db.Integer, primary_key=True)
@@ -12,4 +13,20 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
     phone_number = db.Column(db.String(10), nullable=False)
+
+    def to_dict(self):
+        return{
+            'user_id': self.user_id,
+            'firstName': self.firstName,
+            'lastName': self.lastName,
+            'role': self.role,
+            'email': self.email,
+            'password': self.password,
+            'phone_number': self.phone_number,
+        }
+    
+    def __repr__(self):
+        return f'User(user_id={self.user_id}, firstName={self.firstName}, lastName={self.lastName})'
+
+
     
