b51f203a09e8de34bdb3670d8f7af89da0b3ddeb
Medrine-Jepkemoi <99050771+Medrine-Jepkemoi@users.noreply.github.com>
2023-09-18 15:43:27 +0300

    Login with token

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
 from models import db, User
+import jwt
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
@@ -10,6 +11,36 @@ app.config["DEBUG"] = True
 migrate = Migrate(app, db)
 
 db.init_app(app)
+
+# Token Generation
+def encode_user():
+    """
+    encode user payload as a jwt
+    :param user:
+    :return:
+    """
+    encoded_data = jwt.encode(payload={"name": "Medrine"},
+                              key='secret',
+                              algorithm="HS256"
+                              )
+
+    return encoded_data
+
+
+if __name__ == "__main__":
+    print(encode_user())
+
+def decode_user(token: str):
+    """
+    :param token: jwt token
+    :return:
+    """
+    decoded_data = jwt.decode(jwt=token,
+                              key='secret',
+                              algorithms=["HS256"])
+
+    print(decoded_data)
+
 
 # INDEX ROUTE
 @app.route('/')
@@ -74,20 +105,13 @@ def login():
 
     if not uemail or not upassword:
         response = make_response(jsonify({'error': 'Invalid email or password.'}), 401)
-        return response
+        return response   
     
+    create_token = encode_user()
     
-    response = make_response(jsonify({'message': 'Login successful.'}), 200)
+    response = make_response(jsonify({'message': 'Login successful.', 'access_token': create_token}), 200)
     return response
 
     
-
-
-    
-
-
-    
-    
-
 if __name__ == "__main__":
     app.run(port=5555)
